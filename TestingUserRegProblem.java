27772c4cd15835d9b23492f47f3a3fc3f16a87b5
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-11-01 07:00:07 -0800

    JUNIT : Last Name Must Start With Capital Letter And Have Minimum Three Characters

--- TestingUserRegProblem.java
+++ TestingUserRegProblem.java
@@ -5,15 +5,29 @@ public class TestingUserRegProblem {
 
     @Test
 
-    public void testingFirstName(){
+    public void testingFirstName() {
         UserRegistration userRegistration = new UserRegistration();
         boolean isFNameValid = userRegistration.checkFName("Shubh");
         Assert.assertTrue(isFNameValid);
     }
+
     @Test
-    public void testFirstNameInvalidMustReturnFalse(){
+    public void testFirstNameInvalidMustReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
         boolean isFnameValid = userRegistration.checkFName("shubh");
         Assert.assertFalse(isFnameValid);
+    }
+
+    @Test
+    public void testLastNameValidMustReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean isLnameValid = userRegistration.Lname("Kumar");
+        Assert.assertTrue(isLnameValid);
+    }
+
+    public void testLastNameValidMustReturnFalse() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean isLnameINValid = userRegistration.Lname("kumar");
+        Assert.assertFalse(isLnameINValid);
     }
 }
--- UserRegistration.java
+++ UserRegistration.java
@@ -3,4 +3,8 @@ public class UserRegistration {
         return (fName.matches("[A-Z][a-z]{3,}"));
 
     }
+
+    public boolean Lname(String Lname) {
+        return (Lname.matches("[A-Z][a-z]{3,}"));
+    }
 }
