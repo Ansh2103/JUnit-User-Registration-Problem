76295e75de93a5dfe66d56774e3ae07e6e556ba2
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-11-02 20:47:54 -0800

    Refactor : added functionality to add custom exception

--- testSampleEmails.java
+++ testSampleEmails.java
@@ -9,9 +9,14 @@ import java.util.Collection;
 @RunWith(Parameterized.class)
 public class testSampleEmails {
     private String emails;
+<<<<<<< HEAD
     private boolean expectedResult;
 
     public testSampleEmails(String emails, boolean expectedResult) {
+=======
+    private String expectedResult;
+    public testSampleEmails(String emails, String expectedResult) {
+>>>>>>> UC12/Custom_Exception
         this.emails = emails;
         this.expectedResult = expectedResult;
     }
@@ -20,6 +25,7 @@ public class testSampleEmails {
     public static Collection input()
     {
         return Arrays.asList(new Object[][]{
+<<<<<<< HEAD
                 {"abc@yahoo.com", true},
                 {"abc-100@yahoo.com", true},
                 {"abc.100@yahoo.com", true},
@@ -41,12 +47,35 @@ public class testSampleEmails {
                 {"abc.@gmail.com", false},
                 {"abc@abc@gmail.com", false},
                 {"abc@gmail.com.1a", false}
+=======
+                {"abc@yahoo.com", "Success"},
+                {"abc-100@yahoo.com", "Success"},
+                {"abc.100@yahoo.com", "Success"},
+                {"abc111@abc.com", "Success"},
+                {"abc-100@abc.net", "Success"},
+                {"abc.100@abc.com.au", "Success"},
+                {"abc@1.com", "Success"},
+                {"abc@gmail.com.com", "Success"},
+                {"abc+100@gmail.com", "Success"},
+>>>>>>> UC12/Custom_Exception
         });
     }
 
     @Test
     public void testEmailIds() {
         UserRegistration userRegistration = new UserRegistration();
+<<<<<<< HEAD
         Assert.assertEquals(expectedResult, userRegistration.checkEmail(emails));
+=======
+        String email = null;
+        try {
+            email = userRegistration.checkEmail(emails);
+        }
+        catch (InvalidDetailExceptions e)
+        {
+            Assert.assertEquals(expectedResult, email);
+        }
+
+>>>>>>> UC12/Custom_Exception
     }
 }
